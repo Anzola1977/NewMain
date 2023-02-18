fbafc28fa9d490e773f8acaac394c64f49f2ceb0
Andrew Zola <and.zola.77@gmail.com>
2023-02-18 17:52:37 +0100

    commit with PreparedStatement - working

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,28 +6,24 @@ public class Main {
 
     public static void main(String[] args) {
         final String URL = "jdbc:postgresql://localhost:5432/postgres";
-
         String username = "postgres";
-        String sql = "SELECT * FROM test";
         String password = "ZAPlmk2014";
         String conok = "Соединение с бд установлено";
         String conerr = "Произошла ошибка подключения к бд";
+        int count = 9;
+        String SQL = "Select * from Test WHERE id = ?;";
 
-//        Connection connection = null;
-        try (Connection connection = DriverManager.getConnection(URL, username, password);
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(sql)){
+        try {
+            Connection connection = DriverManager.getConnection(URL, username, password);
             System.out.println(String.format("%s", conok));
-            boolean isExecuted = statement.execute(sql);
-            if (isExecuted) {
-                System.out.println("Select executed");
-            }
-            System.out.println("ID");
-            System.out.println("||------------||");
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+            preparedStatement.setInt(1, count);
+            ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
-                System.out.println(resultSet.getInt("ID"));
+                System.out.println(resultSet.getInt("id") + " | " + resultSet.getString("Name"));
             }
-            System.out.println("||------------||");
+            preparedStatement.close();
+            connection.close();
         } catch (SQLException e) {
             System.out.println(String.format("%s", conerr));
             e.printStackTrace();
